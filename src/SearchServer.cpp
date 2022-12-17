97fffba137d05c21aa4d1f9005cf672098fa8900
maks-lankaster <73167795+maks-lankaster@users.noreply.github.com>
2022-12-17 11:45:57 +0300

    changed search function in SearchServer, new functions: getUniqueWordsFromRequest, getUniqueWordsOccurrencesInDocs, getDocsAbsoluteRelevance ALL TESTS WORK OK

--- src/SearchServer.cpp
+++ src/SearchServer.cpp
@@ -51,43 +51,71 @@ std::vector<std::pair<std::string, std::size_t>> getUniqueWordsOccurrencesInDocs
     return uniqueWordsOccurrencesSorted;
 }
 
+std::map<std::size_t, float> getDocsAbsoluteRelevance(const std::vector<std::pair<std::string, size_t>>& uniqueWords,
+                                                      const InvertedIndex& iIndex) {
+    std::map<std::size_t, float> docsAbsRelevance;
+    for (const auto& currentWord: uniqueWords) { //Loop through sorted words of one request
+        std::vector<Entry> entries = iIndex.getWordCount(currentWord.first); //Get all docs (Entry objects) with the current word
+        for (auto currentEntry: entries) { //Loop through docs (Entry objects) with the current word
+            float relevanceAbs = 0;
+            for (const auto& otherWord: uniqueWords) { //Loop through sorted words of one request to compare with the current word
+                std::vector<Entry> otherEntries = iIndex.getWordCount(
+                        otherWord.first); //Get all docs (Entry objects) with the 'other' word
+                for (auto otherEntry : otherEntries) { //Loop through 'other' docs
+                    if (otherEntry.docId == currentEntry.docId) { //Find if 'other' word appears in the 'current' word docs
+                        relevanceAbs += otherEntry.count; //Increase relevance of the 'current' doc
+                        break;
+                    }
+                }
+            }
+            docsAbsRelevance.insert({currentEntry.docId, relevanceAbs});
+        }
+    }
+    return docsAbsRelevance;
+}
+
+/*std::vector<RelativeIndex> makeRelativeIndex(const std::map<std::size_t, float>& docsAbsRelevance) {
+    std::vector<RelativeIndex> relInd;
+    for(auto doc: docsAbsRelevance) {
+        RelativeIndex ri{};
+        ri.docId = doc.first;
+        ri.rank = doc.second;
+        relInd.push_back(ri);
+    }
+    //SORTING BY RANK AND BY DOCID (IF RANK IS EQUAL)
+    std::sort(relInd.begin(), relInd.end(), [](RelativeIndex &a, RelativeIndex &b) {
+        if (a.rank == b.rank) {
+            return a.docId < b.docId;
+        } else {
+            return a.rank > b.rank;
+        }});
+    float maxAbsRelevance = relInd[0].rank;
+    //Define number of answers based on maxResponses from config.json file
+    int responsesLimit = defineNumberOfResponses(relInd.size());
+    relInd.resize(responsesLimit);
+    for (int i = 0; i < relInd.size(); i++) {
+        relInd[i].rank /= maxAbsRelevance;
+    }
+}*/
+
 std::vector<std::vector<RelativeIndex>> SearchServer::search(const std::vector<std::string> &requests) const {
     std::vector<std::vector<RelativeIndex>> answers;
     for (const auto& request : requests) {
-        //Creating a list of unique words in one request
-        std::unordered_set<std::string> uniqueWords = getUniqueWordsFromRequest(request);
-
-        //Counting total occurrences of a unique word in all documents
-        std::vector<std::pair<std::string, std::size_t>> uniqueWordsOccurrencesSorted = getUniqueWordsOccurrencesInDocs(uniqueWords, iIndex);
-
-        //Sorting unique words by occurrences in all documents (from fewer to greater)
+        auto uniqueWords = getUniqueWordsFromRequest(request); //Creating a list of unique words in one request
+        auto uniqueWordsOccurrencesSorted =
+                getUniqueWordsOccurrencesInDocs(uniqueWords, iIndex); //Counting total occurrences of a unique word in all documents sorted (from smaller to larger)
         if(!uniqueWordsOccurrencesSorted.empty()) {
-            std::map<std::size_t, float> docsFound;
-            for (const auto& currentWord: uniqueWordsOccurrencesSorted) { //Loop through sorted words of one request
-                std::vector<Entry> entries = iIndex.getWordCount(currentWord.first); //Get all docs (Entry objects) with the current word
-                for (auto currentEntry: entries) { //Loop through docs (Entry objects) with the current word
-                    float relevanceAbs = 0;
-                    for (const auto& otherWord: uniqueWordsOccurrencesSorted) { //Loop through sorted words of one request to compare with the current word
-                        std::vector<Entry> otherEntries = iIndex.getWordCount(
-                                otherWord.first); //Get all docs (Entry objects) with the 'other' word
-                        for (int i = 0; i < otherEntries.size(); i++) { //Loop through 'other' docs
-                            if (otherEntries[i].docId == currentEntry.docId) { //Find if 'other' word appears in the 'current' word docs
-                                relevanceAbs += otherEntries[i].count; //Increase relevance of the 'current' doc
-                                break;
-                            }
-                        }
-                    }
-                    docsFound.insert({currentEntry.docId, relevanceAbs});
-                }
-            }
+            std::map<std::size_t, float> docsAbsRelevance =
+                    getDocsAbsoluteRelevance(uniqueWordsOccurrencesSorted, iIndex);
+            ///////////
+            //////////
             std::vector<RelativeIndex> relInd;
-            for(auto it: docsFound) {
+            for(auto doc: docsAbsRelevance) {
                 RelativeIndex ri{};
-                ri.docId = it.first;
-                ri.rank = it.second;
+                ri.docId = doc.first;
+                ri.rank = doc.second;
                 relInd.push_back(ri);
             }
-
             //SORTING BY RANK AND BY DOCID (IF RANK IS EQUAL)
             std::sort(relInd.begin(), relInd.end(), [](RelativeIndex &a, RelativeIndex &b) {
                 if (a.rank == b.rank) {
@@ -95,17 +123,19 @@ std::vector<std::vector<RelativeIndex>> SearchServer::search(const std::vector<s
                 } else {
                     return a.rank > b.rank;
                 }});
-
             float maxAbsRelevance = relInd[0].rank;
-
             //Define number of answers based on maxResponses from config.json file
             int responsesLimit = defineNumberOfResponses(relInd.size());
-
             relInd.resize(responsesLimit);
-
             for (int i = 0; i < relInd.size(); i++) {
                 relInd[i].rank /= maxAbsRelevance;
             }
+
+
+            /////////
+            /////////
+
+            //std::vector<RelativeIndex> relativeIndex = makeRelativeIndex(docsAbsRelevance);
             answers.push_back(relInd);
         } else {
             std::vector<RelativeIndex> ri{};
