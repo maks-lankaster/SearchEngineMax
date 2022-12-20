e7bfc47dfae1a5e0c93703d52ed57e985c14f5b4
maks-lankaster <73167795+maks-lankaster@users.noreply.github.com>
2022-12-20 23:35:42 +0300

    added creation and checking of answers.json file at start added some info outputs to comment the execution of the program ALL TESTS WORK OK

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ void starting() {
     if(!configFile.is_open()) {
         throw ConfigFileIsMissingException();
     }
+    std::cout << "Reading config file..." << std::endl;
     configFile >> configData;
     if (configData.empty()) {
         throw ConfigFileIsEmptyException();
@@ -21,7 +22,19 @@ void starting() {
     if (APP_VERSION != configData["config"]["version"]) {
         throw IncorrectVersionException();
     }
-    std::cout << "WELCOME TO " << std::string(configData["config"]["name"]) << std::endl;
+    std::ifstream iAnswers("answers.json"); //checking if answers.json exits at all
+    if (iAnswers.is_open()) {
+        std::filesystem::permissions("answers.json", std::filesystem::perms::owner_write); //change attrib to owner-write
+        iAnswers.close();
+        std::cout << "Erasing contents of the 'answers.json' file..." << std::endl;
+        std::ofstream oAnswers("answers.json"); //open file for output and erase its contents
+        oAnswers << "";
+        oAnswers.close();
+    } else {
+        std::cout << "Creating 'answers.json' file..." << std::endl;
+        std::ofstream oAnswers("answers.json"); //creating an empty 'answers.json' file
+    }
+    std::cout << std::endl << "WELCOME TO " << std::string(configData["config"]["name"]) << std::endl << std::endl;
 }
 
 std::vector<std::vector<std::pair<int, float>>> convertFromRelativeIndex(std::vector<std::vector<RelativeIndex>>& answersRelativeIndex) {
@@ -49,6 +62,8 @@ int main() {
         return 0;
     }
 
+    std::cout << "Search started..." << std::endl;
+
     //Creating InvertedIndex object
     InvertedIndex invertedIndex;
 
@@ -73,4 +88,5 @@ int main() {
     //Converting answers of type RelativeIndex to type std::pair<std::size_t, float> to use with putAnswers function
     auto answersPair = convertFromRelativeIndex(answersRelativeIndex);
     ConverterJSON::putAnswers(answersPair);
+    std::cout << std::endl << "PROGRAM FINISHED CORRECTLY" << std::endl;
 }
